2452087f0a47d18a31ab3d1d75fe1b520f1dbd65
john leal <voltoja35@gmail.com>
2024-10-08 17:06:25 -0300

    repetical

---
+++ Documents/estrutu-repetical 2/js/ex 1.js
@@ -0,0 +1,16 @@
+
+// 3. Escreva um algoritmo que receba um valor inicial e um valor final, ambos informados pelo usuário. Mostre todos os valores ímpares, do maior para o menor.
+
+// Exemplo:
+//  valor inicial: 20
+//  valor final: 200
+//  Resultado: 199, 197, 195... 25, 23, 21.
+
+// usuario informa os valores inicial e final
+// const valorInicial = prompt`informe o valor inicial`;
+// const valorFinal = prompt`informe o valor final`;
+// for (let i = valorFinal; i >= valorInicial; i--){
+//     if (i % 2 !=0)
+//         console.log(i);
+        
+// }
